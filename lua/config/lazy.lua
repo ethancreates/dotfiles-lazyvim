e38c42080cd7364fb2a72d3f9b351ab560130a45
ethancreates-pc <ethancreates@proton.me>
2024-04-20 20:10:25 +0800

    added toggleterm and edgynvim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,7 +20,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.vscode" },
-
+    { import = "lazyvim.plugins.extras.ui.edgy" },
     -- import/override with your plugins
     { import = "plugins" },
   },
