44faf956057adea73903d855b995046f2bc9b230
ethancreates-pc <ethancreates@proton.me>
2024-05-09 22:28:23 +0800

    using lazygit instead of gitui

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,7 +20,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.vscode" },
-    { import = "lazyvim.plugins.extras.util.gitui" },
     -- import/override with your plugins
     { import = "plugins" },
   },
